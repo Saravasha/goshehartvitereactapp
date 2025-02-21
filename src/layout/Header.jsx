4a409dacd234e853e403796cadf23fdae1a2ceb7
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-02-21 02:23:45 +0100

    added coming soon

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,14 +3,13 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+ 
 
-    <div className="solid-border">
-      <span className="solid-border">This is a Header</span>
-      <img src="https://picsum.photos/200/200" alt="" id="1" className="w-1"/>
+    <div className="solid-border border-black h flex items-center">
+      <span className="text-9xl flex align-middle">Gosheh Art</span>
+      {/* <img src="https://picsum.photos/200/200" alt="" id="1" className="w-1"/> */}
     </div>
 
-    </header>
   
   );
 }
