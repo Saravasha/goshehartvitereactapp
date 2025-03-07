183ec80d9838a4860b622d01c004df205ef6b88b
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-03-07 20:29:01 +0100

    adding .env files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/', 
   plugins: [react()],
   'process.env': {
-    DOTNET_API_URL: process.env.DOTNET_API_URL || 'https://admin.goshehart.se/api/react/asset',
+    DOTNET_API_URL: process.env.VITE_DOTNET_API_URL || 'https://admin.goshehart.se/api/react/asset',
+    DOTNET_API_URL_DEV: process.env.VITE_DOTNET_API_URL_DEV || 'http://localhost:5283/api/react/asset',
   }
 })
