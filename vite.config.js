288a344b2628e932fbe5cbbc36fe2205d0892173
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-04-02 02:08:14 +0200

    aweawe

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), react()],
   "process.env": {
     DOTNET_ASSET_API_URL:
       process.env.VITE_DOTNET_ASSET_API_URL ||
