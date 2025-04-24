9d2f5c39aa9c6ec786e852d95d82830871413f5e
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-04-24 21:43:58 +0200

    framer-motion added on app and loadingScreen component

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import bgimg from "../assets/comingsoonimg3.jpg";
+import { motion, AnimatePresence } from "framer-motion";
 
 const LoadingScreen = () => {
   return (
-    <div className="w-screen h-screen  -z-1 flex bg-gradient-to-r from-green-900 to-green-500 top-0 flex-col gap-8 p-4  items-center justify-center    shadow-2xl">
+    <motion.div
+      className="w-screen h-screen flex bg-gradient-to-r from-green-900 to-green-500 flex-col gap-8 p-4 items-center justify-center shadow-2xl relative overflow-hidden"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, y: -100 }}
+      transition={{ duration: 1 }}
+    >
+      {/* <div className="w-screen h-screen  -z-1 flex bg-gradient-to-r from-green-900 to-green-500 top-0 flex-col gap-8 p-4  items-center justify-center    shadow-2xl"> */}
       <span className="mt-8 text-6xl z-1 sm:text-7xl md:text-9xl font-thin text-white  text-center">
         Gosheh Art
       </span>
@@ -21,7 +29,8 @@ const LoadingScreen = () => {
         <div className="w-6 h-6 bg-white rounded-full delay-300 animate-delay" />
       </div>
       <div className="w-screen"></div>
-    </div>
+      {/* </div> */}
+    </motion.div>
   );
 };
 export default LoadingScreen;
