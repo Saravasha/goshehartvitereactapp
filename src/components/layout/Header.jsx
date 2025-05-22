17ddba197b8ce30e6a1350e66b3b7d9f81f34476
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-05-22 19:34:28 +0200

    aweawe

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import useColors from "../../components/color/useColors.jsx";
+import { useData } from "../../components/api/ApiContext.jsx";
 
 export default function Header() {
+  const { colors } = useData();
+  const colorInStyle = useColors(colors, "Header Text") || {};
   return (
     <div className="flex italic  justify-center font-thin ">
-      <p className="m-4 p-4 text-shadow-2xl text-9xl hover:text-green-700 hover:animate-pulse dark:text-white text-black">
+      <p
+        className="m-4 p-4 text-shadow-2xl text-9xl hover:text-green-700 hover:animate-pulse drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+        style={colorInStyle}
+      >
         Gosheh Art
       </p>
     </div>
