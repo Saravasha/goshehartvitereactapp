e2d87009000d59931c14e67c85a8f05e2619b547
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-05-30 22:03:46 +0200

    aweaweawe

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,7 +1,8 @@
+import comingSoon from "../../comingsoonimg2.jpg";
 export default function ComingSoon() {
   return (
     <div className="ComingSoonBody relative justify-center flex w-full h-full ">
-      <img src="/comingsoonimg2.jpg" className="rounded-full h-auto" alt="ph" />
+      <img src={comingSoon} className="rounded-full h-auto" alt="ph" />
       <p className="absolute inset-0 italic flex justify-center align-middle text-shadow-rose-200 text-red-50  text-8xl pt-5">
         Gosheh Art
       </p>
