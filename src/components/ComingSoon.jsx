9f93e268a496cf4396603aa7c4d0fbac0c08321f
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-06-03 09:44:46 +0200

    final touches

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -3,12 +3,12 @@ export default function ComingSoon() {
   return (
     <div className="ComingSoonBody relative justify-center flex w-full h-full ">
       <img src={comingSoon} className="rounded-full h-auto" alt="ph" />
-      <p className="absolute inset-0 italic flex justify-center align-middle text-shadow-rose-200 text-red-50  text-8xl pt-5">
+      {/* <p className="absolute inset-0 italic flex justify-center align-middle text-shadow-rose-200 text-red-50  text-8xl pt-5">
         Gosheh Art
-      </p>
-      <p className="absolute inset-0 italic flex justify-center items-center-safe text-shadow-rose-200 text-red-50  text-4xl">
+      </p> */}
+      {/* <p className="absolute inset-0 italic flex justify-center items-center-safe text-shadow-rose-200 text-red-50  text-4xl">
         Coming soon...
-      </p>
+      </p> */}
     </div>
   );
 }
